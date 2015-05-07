2812b30d892f34ad9e9c1c06c70fdc0c220f24d1
Joseph McDonald <josephpmcdonald@gmail.com>
2015-05-07 17:42:25 -0400

    added impurity macros, most of branching, and fixed some errors

--- tree.c
+++ tree.c
@@ -2,26 +2,43 @@
 #include <stdlib.h>
 #include <math.h>
 
+/* Notes: Use main to test functions.
+ * 
+ * TODO:
+ * In BestSplit, take care of case when i=n before computing impurity, else division by 0
+ *
+ *
+ */
+
+
+#define GINI(pos, n) (2.*(pos)/(n)*((n)-(pos))/(n))
+#define ENTROPY(pos, n) ((pos)/(n)*log((pos)/(n)) + ((n)-(pos))/(n)*log(((n)-(pos))/(n)))
 
 const int classes = 2;
 const int N = 1000;
 const int d = 10;
 
-struct node {
-    struct node *p;
-    int ind;
-    double split;
-    struct node *right;
-    struct node *left;
+struct Node {
+    struct Node *parent;
+    struct Node *right;
+    struct Node *left;
+    int index;
+    double threshold;
+    double class;
 };
 
 
-void Sort(float *data[][d], int first, int last, int a) {
+void Sort(float data[][d], int first, int last, int a) {
 
 /* Sort and Partition implements quicksort and sorts a 2-dimensional
- * array on a particular index a. Sort places entries matching
- * the pivot next to the pivot in the sorted array to minimize
+ * array on a particular index a between indices first and last. Sort places
+ * entries matching the pivot next to the pivot in the sorted array to minimize
  * recursive Sort calls in the case of repeated values.
+ *
+ * data  = array containing sample data and labels to be sorted
+ * first = first unsorted row that Sort may move
+ * last  = last unsorted row that Sort may move
+ * a     = index to sort on
  *
  */
 
@@ -31,12 +48,13 @@ void Sort(float *data[][d], int first, int last, int a) {
     //Save the ends of the unsorted portions in vector.
     int ends[2];
 
-    Partition(*data, first, last, a, ends);
+    Partition(data, first, last, a, ends);
     Sort(data, first, ends[0], a);
     Sort(data, ends[1], last, a);
     
     return;
 }
+
 
 void Partition(float data[][d], int first, int last, int a, int ends[]) {
 
@@ -44,8 +62,15 @@ void Partition(float data[][d], int first, int last, int a, int ends[]) {
  * rows that match the pivot value together to minimize recursive calls
  * to Sort. 
  *
+ * data  = array containing sample data and labels to be sorted
+ * first = first unsorted row that Sort may move
+ * last  = last unsorted row that Sort may move
+ * a     = index to sort on
+ * ends  = vector with last index of lower unsorted block and first 
+ *         index of higher unsorted block
  *
  */
+
     double pivot = data[last][a];
     int i = first;
     int j = last-1;
@@ -110,21 +135,22 @@ void Partition(float data[][d], int first, int last, int a, int ends[]) {
 }
 
 
-int split(float data[][d], int n, int a, int pos) {
+int BestSplit(float data[][d], int n, int first, int col, int pos, double *impurity) {
 
-/* Returns the index of the table with best threshold split.
- * Partition rows up to and including that index from
- * everything afterwards.
+/* Returns the index of the table with the least impurity
+ * after splitting. Partition rows up to and including that
+ * index from everything afterwards.
  *
- * data = node data
- * n = length of data
- * a = index to split on
- * pos = number of positive labels
+ * data = array of data sorted on index a 
+ * n    = length of table (# of rows/samples)
+ * col    = sorting/splitting feature/column of data
+ * pos  = number of positive labels
  *
  * Thus:
  * (pos - lpos) = right pos count
  * i = total left count
  * n - i = total right count
+ * 
  *
  */
 
@@ -136,82 +162,125 @@ int split(float data[][d], int n, int a, int pos) {
 
     double threshold;
     double P;
-    double argmin;
+    int argmin = -1;
     double threshmin;
-    double Pmin;
-    Pmin = 2.*(pos/n)*(neg/n); //initial purity of node
+    double Pmin = GINI(pos, n);//initial impurity of node
 
-    //Tabulate purity for each possible threshold split    
+    //Tabulate impurity for each possible threshold split    
     i = 0;
     while (i < n) {
 
-        threshold = data[i][a];
+        threshold = data[first+i][col];
 
-        while (data[i][a] == threshold) {
-            if (data[i][d-1] == 1)
+        while (data[first+i][col] == threshold) {
+            if (data[first+i][d-1] == 1)
                 lpos += 1;
             ++i;
         }
 
-        //right = n - i;
-        //lneg = i - lpos;
-
         //Note that points on left = i, right = n-i
-        lp = (double) lpos/i;
-        rp = (double) (pos - lpos)/(n - i);
 
-        //P = 2mu_l*lp(1-lp) + 2mu_r*rp(1-rp)
-        P = 2.*lpos/n*(1 - lp) + 2.*(pos - lpos)/n*(1 - rp);
+        //If i=n, this is the whole node and the impurity is initial
+        if (i < n) {
+            P = GINI(lpos, i)*i/n + GINI(pos-lpos, n-i)*(n-i)/n;
 
-        //Split data at threshold/index with min purity
-        if (P < Pmin) {
-            Pmin = P;
-            argmin = i - 1;
-            threshmin = threshold;
+            //Save threshold/index with min impurity
+            if (P < Pmin) {
+                Pmin = P;
+                argmin = i - 1;
+                threshmin = threshold; 
+            }
         }
     }
 
+    //Save the minimum impurity to compare against other indices
+    *impurity = Pmin;
     return argmin;
 }
 
 
-struct node *buildTree( ) {
+void SplitNode(struct Node *node, float data[][d], int n, int first) {
 
+/* Creates two branches of the decision tree on the array data. End condition
+ * creates leaf if the purity of the node is small or if there are few
+ * samples on the branch of node
+ *
+ * node  = pointer to node in decision tree
+ * data  = table of unsorted data with features and labels (with last
+ *         column as the label (data[i][d-1]))
+ * n     = length of table (# of rows/samples) on branch of node
+ * first = first index of samples on branch of node
+ */
 
-    //Get the initial counts for positive/negatives.
-    int i = 0;
+    //Get initial counts for positive/negative labels
+    int i;
+    int pos;
     for (i = 0; i < n; ++i) {
-        if (data[d*i + d-1] == 1.)
+        if (data[first+i][d-1] == 1.)
             pos += 1;
     }
+    int neg = n - pos;
 
-    if (Pmin < 0.05 || n < 10) {
-        struct node *leaf;
-        
+    //Declare class for node in case of pruning on child
+    if (pos > neg)
+        node->class = 1;
+    else if (pos < neg)
+        node->class = -1;
+    else if (node->parent)
+        node->class = node->parent->class;
+    else
 
-        return leaf;
+    //If branch is small or almost pure, make leaf
+    if (n < 6 || GINI(pos, n) < 0.01) {
+        node->left = NULL;
+        node->right = NULL;
+        return;
     }
 
-    int a;
-    double Pmin = 100;
-    double threshmin; 
 
-    for (i = 0; i < d-1; ++a) {
-        P = split(data, n, i, pos);
-        if (P < Pmin) {
-            a = i;
-            Pmin = P;
-            threshmin = data[a][];
+    int col;
+    int row; //best row to split at for particular column/feature
+    double threshold; //best threshold to split at for column/feature
+    double impurityaddress;
+    double *impurity = &impurityaddress; //pointer to impurity for feature
+    int bestcol; //feature with best split
+    double bestthresh; //threshold split for best feature
+    double Pmin = 100; //minimum impurity seen so far
+
+    //Sort table. Then find best column/feature, threshold, and impurity
+    for (col = 0; col < d-1; ++col) {
+        Sort(data, first, first+n-1, col);
+        row = BestSplit(data, n, first, col, pos, impurity);
+        threshold = data[row][col];
+        
+        //If current column has better impurity, save col, thresh, and Pmin
+        if (*impurity < Pmin) {
+            bestcol = col;
+            bestthresh = threshold;
+            Pmin = *impurity;
         }
     }
 
+    Sort(data, first, first+n-1, bestcol);
 
-    struct node *Node = malloc(sizeof *Node);
-    Node->p = parent;
-    Node->ind = a;
-    Node->split = threshmin;
+    //For feature, threshold with best impurity, save to node attributes
+    node->index = bestcol;
+    node->threshold = bestthresh;
 
-    //copy the data into     
+    //Create right and left children
+    struct Node right;
+    struct Node left;
+    right.parent = node;
+    left.parent = node;
+    node->right = &right;
+    node->left = &left;
+    
+
+}
+
+
+
+/*
     double *ldata = malloc(argmin * sizeof(double*));
     double *rdata = malloc((n - argmin) * sizeof(double*));
     double *lmatrix = malloc(argmin * d * sizeof(double));
@@ -231,14 +300,12 @@ struct node *buildTree( ) {
         for (j = 0; j < d; ++j)
             rdata[i][j] = data[i + argmin][j];
     }
-}
+*/
 
 
 
 
-
-
-struct node* countsplit(float data[][d], int n, int a, node *) {
+struct node* CountSplit(float data[][d], int n, int a, node *) {
 
 /* data = node data
  * n = length of data
